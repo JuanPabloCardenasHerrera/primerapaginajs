5ea24827af3a6d158a44d81dbcc563485a4aa60c
Juan Pablo Cardenas Herrera <jcardenas@ucb.edu.bo>
2021-08-27 01:33:27 -0400

    adicion de la edad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const nombre = document.querySelector("#input_name");
 const form = document.querySelector("#greetings_form");
-
+const edad = document.querySelector("#input_age");
 form.addEventListener("submit",(event)=>{
-	alert("Hola" + nombre.value);
+	alert("Hola " + nombre.value + edad.value);
 });
