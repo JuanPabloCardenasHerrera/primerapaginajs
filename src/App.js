ccd1afae7f6f8711d0ee6316b762bf5d0bd08781
Juan Pablo Cardenas Herrera <jcardenas@ucb.edu.bo>
2021-08-27 01:43:38 -0400

    adicion de la edad con condicional

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@ const nombre = document.querySelector("#input_name");
 const form = document.querySelector("#greetings_form");
 const edad = document.querySelector("#input_age");
 form.addEventListener("submit",(event)=>{
-	alert("Hola " + nombre.value + edad.value);
+	if(edad.value<=0){
+		alert("Hola " + nombre.value + " sin edad");	
+	}
+	else{
+		alert("Hola " + nombre.value + " " + edad.value);
+	}
+
 });
